9a479b650ce373b0257586d6e9713588b73f1dad
AlderFurtado <aldermarcel26@outlook.com>
2021-03-30 01:52:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [birthYear, setBirthYear] = useState(1997);
+  const [age, setAge] = useState(2021 - 1997);
+
+  // //componentDidUpdate
+  // //  - Quando o componente é atualizado (mudança de state)
+  useEffect(() => {
+    alert("Esse componente foi atualizado");
+  });
+
+  // //componentDidMount
+  // //  - Quando o componente é montado
+  useEffect(() => {
+    alert("Esse componente foi montado e esse aviso só aparecerá uma vez");
+  }, []);
+
+  // //componentWillUnmount
+  // //  - Quando o componente é desmontado
+  useEffect(() => {
+    return () => {
+      alert("Esse componente foi desmontado");
+    };
+  });
+
+  // Escuta a atualização exclusiva de um elemento dentro do component
+  useEffect(() => {
+    setAge(2021 - birthYear);
+  }, [birthYear]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ano atual: 2021</h1>
+      <h1>Ano de nascimento: {birthYear}</h1>
+      <div>
+        <button onClick={() => setBirthYear(birthYear - 1)}>-</button>
+        <button onClick={() => setBirthYear(birthYear + 1)}>+</button>
+      </div>
+      <h1>Idade: {age}</h1>
     </div>
   );
 }
